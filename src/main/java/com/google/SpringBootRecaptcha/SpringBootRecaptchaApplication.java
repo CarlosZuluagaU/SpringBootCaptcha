d4bf8bb14e310d78eefbd66454697a6d2b157325
Carlos Zuluaga <andres.zuluaga6@udea.edu.co>
2025-07-23 08:45:15 -0500

    v1.0 and first version, create jpa setting, db and pom.xml

---
+++ src/main/java/com/google/SpringBootRecaptcha/SpringBootRecaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.google.SpringBootRecaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecaptchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/SpringBootRecaptcha/SpringBootRecaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.SpringBootRecaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
